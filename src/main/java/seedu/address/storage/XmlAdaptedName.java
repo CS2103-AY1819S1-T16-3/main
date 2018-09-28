b25b7e1717b3a9b4954651189468061ec4b819e0
Jin Ying <developer.tjy@gmail.com>
2018-09-29 00:25:13 +0800

    Create XmlAdaptedName.java

---
+++ src/main/java/seedu/address/storage/XmlAdaptedName.java
@@ -0,0 +1,61 @@
+package seedu.address.storage;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Name;
+
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ * JAXB-friendly adapted version of the Name.
+ */
+public class XmlAdaptedName {
+    @XmlValue
+    private String name;
+
+    /**
+     * Constructs an XmlAdaptedName.
+     * This is the no-arg constructor that is required by JAXB.
+     */
+    public XmlAdaptedName() {}
+
+    /**
+     * Constructs a {@code XmlAdaptedName} with the given {@code name}.
+     */
+    public XmlAdaptedName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Converts a given name into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created
+     */
+    public XmlAdaptedName(Name source) {
+        name = source.fullName;
+    }
+
+    /**
+     * Converts this jaxb-friendly adapted name object into the model's Name object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted order
+     */
+    public Name toModelType() throws IllegalValueException {
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_NAME_CONSTRAINTS);
+        }
+        return new Name(name);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof XmlAdaptedName)) {
+            return false;
+        }
+
+        return name.equals(((XmlAdaptedName) other).name);
+    }
+}
