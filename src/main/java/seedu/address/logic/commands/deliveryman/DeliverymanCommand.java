fd82ec96a1944b39c18cddcbb255432df114a890
Jin Ying <developer.tjy@gmail.com>
2018-10-02 00:52:32 +0800

    Create DeliverymanCommand.java

---
+++ src/main/java/seedu/address/logic/commands/deliveryman/DeliverymanCommand.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands.deliveryman;
+
+import seedu.address.logic.commands.Command;
+
+/**
+ * Base Command for all order deliveryman command
+ */
+public abstract class DeliverymanCommand extends Command {
+    public static final String COMMAND_WORD = "/deliveryman";
+}
