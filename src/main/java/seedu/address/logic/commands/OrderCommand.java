3f036f239860276be40b51c2bcc3300535735f88
Jin Ying <developer.tjy@gmail.com>
2018-10-01 23:50:39 +0800

    OrderCommand.java: Update style

--- src/main/java/seedu/address/logic/commands/OrderCommand.java
+++ src/main/java/seedu/address/logic/commands/OrderCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
+/**
+ * Base Command for all order commands
+ */
 public abstract class OrderCommand extends Command {
-
     public static final String COMMAND_WORD = "/order";
 }
