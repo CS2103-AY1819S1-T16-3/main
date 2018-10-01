a969c28880acb4564586008ea4f4c08ebd6ba84d
Jin Ying <developer.tjy@gmail.com>
2018-10-01 19:19:24 +0800

    Create FindOrderCommand.java

---
+++ src/main/java/seedu/address/logic/commands/FindOrderCommand.java
@@ -0,0 +1,40 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.order.OrderContainsAnyKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
+
+public class FindOrderCommand extends OrderCommand {
+    public static final String COMMAND_WORD = "find";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Finds the order whose name or phone number contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " p/1234578";
+
+    private final OrderContainsAnyKeywordsPredicate predicate;
+
+    public FindOrderCommand(OrderContainsAnyKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredOrderList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_ORDERS_LISTED_OVERVIEW, model.getFilteredOrderList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindOrderCommand // instanceof handles nulls
+                && predicate.equals(((FindOrderCommand) other).predicate)); // state check
+    }
+}
